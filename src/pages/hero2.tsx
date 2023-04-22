3de93139246c63b7f9f27c64b345489cfbd82b8a
Icaro <icaro.onofre.s@gmail.com>
2023-04-22 00:29:58 -0300

    [wip] aprimorando design da segundan hero page

--- src/pages/hero2.tsx
+++ src/pages/hero2.tsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 
 export function Hero2() {
-return(
-  <div className=" grid grid-cols-12 gap-5 bg-bgblue">
-    {/* Hero section starts here */}
-    <div className="bg-lightblue h-screen right-0 w-28"></div>
+  return (
+    <div className=" grid grid-cols-12 gap-5 bg-bgblue h-full w-full">
+      {/* Hero section starts here */}
 
-    <div className="col-start-3 col-end-4 mt-52">
-      <div className="flex flex-row ">
-        <p className="font-libre text-darkblue text-4xl">Farofa</p>
-        <div className="w-1 h-28 bg-darkblue"></div>
-        <p className="font-roboto font-bold text-darkblue text-4xl">
+      <div className="col-start-8 col-end-9 mt-52">
+        <p className="font-libre text-darkblue text-7xl">Ícaro Onofre Silva.</p>
+        <div className="h-2 w-80 bg-lightblue" />
+        <p className="font-roboto font-bold text-bgblue text-4xl">
           fullstack, frontend, backend
         </p>
       </div>
+      <div className="bottom-0 col-start-1 col-end-13 h-80 ">
+        <div className="h-full bg-black w-72 z-20 ">
+          <p className="font-roboto font-bold text-bgblue text-4xl">
+            fullstack, frontend, backend
+          </p>
+          <div className="w-full h-1/2 z-10 bg-lightblue " />
+          <div className="w-full h-1/2 z-10 bg-bgblue " />
+        </div>
+      </div>
     </div>
-  </div>
-  )}
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,7 @@ module.exports = {
 	    blue:"#16247C",
 	    lightblue:"#1285EF",
 	    bgblue:"#F2F0FF",
+	    black:"#1D1D1B",
     },
     extend: {
 	    fontFamily:{
